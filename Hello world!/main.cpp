05c8ee8e7e01912b2c130af2057e2e94f1463900
Minghao Wang <minghaowang@Minghaos-MacBook-Pro.local>
2013-02-02 10:37:35 -0500

    Initial Commit

---
+++ Hello world!/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello world!
+//
+//  Created by Minghao Wang on 2/2/13.
+//  Copyright (c) 2013 Minghao Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
